433afcb857a4fc851e249238373bdb7206051179
David Huveau <d.huveau@gmail.com>
2018-11-12 21:31:03 +0100

    add CORS middleware

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = 3000;
 
 const studentsRouter = require('./controller')
 
 // middleware
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -19,5 +21,5 @@ app.listen(PORT, (err) => {
     if (err) {
         throw new Error('Something bad happened...');
     }
-    console.log(`Server is listening on ${PORT}`);
+    console.log(`CORS-enabled web server listening on port ${PORT}`);
 });
